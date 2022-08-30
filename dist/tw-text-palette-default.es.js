1ab097896b54c0150e23eaefeb2955ffc707f233
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-30 09:22:25 -0400

    1.2.0 - updated outdated deps and change to automated publishing

--- dist/tw-text-palette-default.es.js
+++ dist/tw-text-palette-default.es.js
@@ -1,12 +1,14 @@
-var twTextPaletteDefault = {
-  "default": {
+const e = {
+  default: {
     colors: {
-      "default": "",
-      "error": "text-rose-500 dark:text-rose-300",
-      "primary": "text-blue-500 dark:text-blue-300",
-      "secondary": "text-violet-500 dark:text-violet-300",
-      "success": "text-green-600 dark:text-green-300"
+      default: "",
+      error: "text-rose-500 dark:text-rose-300",
+      primary: "text-blue-500 dark:text-blue-300",
+      secondary: "text-violet-500 dark:text-violet-300",
+      success: "text-green-600 dark:text-green-300"
     }
   }
 };
-export { twTextPaletteDefault };
+export {
+  e as twTextPaletteDefault
+};
